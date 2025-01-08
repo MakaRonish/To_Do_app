6fd45045026cc070a5baba7613b48ff44be6d2f0
MakaRonish <ronishmakaju12345@gmail.com>
2025-01-07 23:15:10 -0500

    models

--- To_Do/Tasks/models.py
+++ To_Do/Tasks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class tasks(models.Model):
+    task=models.CharField(max_length=100)
+    description=models.TextField()
+    completed=models.BooleanField(default=False)
