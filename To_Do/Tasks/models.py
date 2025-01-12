39bd6a94f0579c2fbd0d80325093929073a9ecce
MakaRonish <ronishmakaju12345@gmail.com>
2025-01-11 23:11:24 -0500

    user

--- To_Do/Tasks/models.py
+++ To_Do/Tasks/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 import uuid
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class tasks(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
     type = [
         ("Work", "Work"),
         ("Personal", "Personal"),
